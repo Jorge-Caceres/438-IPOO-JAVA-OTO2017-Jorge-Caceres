c6bc689ab5961f4a87a7765fa71faa0bfd5bcb94
Jorge-Caceres <jorge97caceres@gmail.com>
2017-05-27 16:49:16 -0400

    Agregado el List y ArrayList

--- practica/src/main/java/com/jorgecaceres/ipoo/practicasRecuMayo/PracticaArrays.java
+++ practica/src/main/java/com/jorgecaceres/ipoo/practicasRecuMayo/PracticaArrays.java
@@ -1,15 +1,46 @@
 package com.jorgecaceres.ipoo.practicasRecuMayo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PracticaArrays {
       private int [] arrayDeEnteros;
+      private List<Integer> listaDeEnteros;
+      
       
       public PracticaArrays(){
         arrayDeEnteros= new int[10];
+        listaDeEnteros = new ArrayList<>();
+      }
+      
+      public void testList(){
+    	  int valor =0;
+    	  int valorMaximo = 10;
+    	  
+    	  for(int i=0; i<valorMaximo; i++){
+    		  valor++;
+    		  listaDeEnteros.add(valor);
+    		  
+    		  
+    	  }
+    	  
+    	  for(Integer lista: listaDeEnteros){
+    		  System.out.println(lista);
+    	  }
+    	  
+    	  listaDeEnteros.remove(5);
+    	  Integer i = listaDeEnteros.get(2);
+    	  listaDeEnteros.remove(i);
+    	  
+    	  for(Integer lista: listaDeEnteros){
+    		  System.out.println(lista);
+    	  }
       }
       
       public static void main(String args[]){
     	  PracticaArrays pa = new PracticaArrays();
     	  pa.test();
+    	  pa.testList();
       }
       
       private void test() {
